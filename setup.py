8bec5be53fb53546bc07a1fe84cc1a7e3c29f450
Eric Nuno <ericnuno@gmail.com>
2018-08-12 20:26:05 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ericnuno',
+    version='0.1',
+    description='Erics common functions',
+    url="https://github.com/kingmold/ericnuno",
+    author='Eric Nuno',
+    author_email='ericnuno@gmail.com',
+    license='ericnuno',
+    packages=['ericnuno'],
+    zip_safe=False)
