5d7420104bab90f90e896502cdf6807931ae0951
kingmold <ericnuno@gmail.com>
2019-10-28 17:36:59 -0500

    Version 0.55

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import setup
 
 setup(name='ericnuno',
-    version='0.54',
+    version='0.55',
     description='Erics common functions',
     url="https://github.com/kingmold/ericnuno",
     author='Eric Nuno',
