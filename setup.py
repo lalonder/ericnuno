55477f89ff6017fc391e714ec73151ba01847813
Eric Nuno <ericnuno@gmail.com>
2019-10-29 11:56:10 -0500

    added serial and changed OS to windows

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import setuptools
 from setuptools import setup
 
 setup(name='ericnuno',
-    version='0.55',
+    version='0.56',
     description='Erics common functions',
     url="https://github.com/kingmold/ericnuno",
     author='Eric Nuno',
     author_email='ericnuno@gmail.com',
     license='MIT',
     packages=setuptools.find_packages(),
-    install_requires=['pyVim', 'pyVmomi', 'requests', 'paramiko', 'bs4'],
+    install_requires=['pyVim', 'pyVmomi', 'requests', 'paramiko', 'bs4', 'serial'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"))
+        "Operating System :: Windows"))
